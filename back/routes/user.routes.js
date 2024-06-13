2bc68b31567c369f716ae410c81df67713c8a843
DWDW2 <daid8002@gmail.com>
2024-06-13 08:41:45 +0500

    finnaly changed this

--- back/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import protectRoute from '../middleware/protectedRoute.middleware.js'
-import { getUsersForSideBar } from '../controllers/user.controllers.js'
-const router = express.Router()
-
-router.get('/Users', protectRoute, getUsersForSideBar)
-
-export default router
